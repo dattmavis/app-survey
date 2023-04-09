1db9e88f0e3145601cc848d31f7964059036b109
Matt Davis <84292097+dattmavis@users.noreply.github.com>
2023-04-09 01:23:45 -0500

    updating files for auth0

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { Auth0Provider } from "@auth0/auth0-react";
+
 import "./style.css";
+
 
 function App() {
   const [components, setComponents] = useState([]);
