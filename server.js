6e781d270c41edc17bc11175c9fde66e5589d0c8
Matt Davis <84292097+dattmavis@users.noreply.github.com>
2023-04-08 22:22:55 -0500

    Update server.js
    editing for linode

--- server.js
+++ server.js
@@ -205,6 +205,6 @@ app.post("/components", async (req, res) => {
   }
 });
 
-app.listen(3001, "198.58.125.182", () => {
+app.listen(3001, "45.33.7.162", () => {
   console.log("Server listening on port 3001");
 });
