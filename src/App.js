8dc6dec5ff4f39ba39ab5cfd5d77424fd1fb1c21
Matt Davis <84292097+dattmavis@users.noreply.github.com>
2023-04-10 15:29:41 -0500

    added navbar

--- src/App.js
+++ src/App.js
@@ -132,89 +132,74 @@ function App() {
 <div className="survey-container">
   {isAuthenticated ? (
     <div>
-      <div className="login-container">
-        <p>User: {user.name}</p>
-        <button className="login-button" onClick={handleLogout}>Log Out</button>
+      <div className="navbar">
+        <h1 className="title">Application Survey</h1>
+        <div className="user-info">
+          <p>User: {user.name}</p>
+          <button className="login-button" onClick={handleLogout}>Log Out</button>
+        </div>
       </div>
       <div className="header-container">
-  <div className="title-container">
-    <h1 className="title">Application Survey</h1>
-    {components.map((component) => (
-      <div key={component.EEID}>
-        <p>
-          <b>Application Name:</b> {component.Name}
-        </p>
-        <p>
-          <b>Description: </b>
-          {component.Description}
-        </p>
-        <p>
-          <b>Architecture: </b>
-          {component.ArchitectureName}
-        </p>
+        <div className="logo-container">
+          <img src="logo.png" alt="Logo" className="logo" />
+        </div>
+        <div className="question-container">
+          {isSubmitted ? (
+            <SubmissionList />
+          ) : (
+            <div>
+              {components.length > 0 && (
+                <form onSubmit={handleSubmit}>
+                  {questions.map((question) => (
+                    <div
+                      key={question.name}
+                      className={
+                        showDescriptions[question.name]
+                          ? "question-card active"
+                          : "question-card"
+                      }
+                    >
+                      <div
+                        className="question-title"
+                        onClick={() => toggleDescription(question.name)}
+                      >
+                        <h2 className="question">{question.name}</h2>
+                        <span className="question-icon">
+                          {showDescriptions[question.name] ? "▲" : "ⓘ"}
+                        </span>
+                      </div>
+                      {showDescriptions[question.name] && (
+                        <div className="question-description">
+                          <p>{question.description}</p>
+                        </div>
+                      )}
+                      <div className="question-answer">
+                        <select
+                          className="answer"
+                          name={question.name}
+                          value={answers[question.name]}
+                          onChange={(e) =>
+                            handleAnswerChange(question.name, e.target.value)
+                          }
+                        >
+                          <option value="">Select An Answer</option>
+                          <option value="5">5 - Perfect fit</option>
+                          <option value="4">4 - Good fit</option>
+                          <option value="3">3 - Average fit</option>
+                          <option value="2">2 - Poor fit</option>
+                          <option value="1">1 - No fit</option>
+                        </select>
+                      </div>
+                    </div>
+                  ))}
+                  <button type="submit">Submit</button>
+                </form>
+              )}
+            </div>
+          )}
+        </div>
+        <hr />
       </div>
-    ))}
-  </div>
-  <div className="logo-container">
-    <img src="logo.png" alt="Logo" className="logo" />
-  </div>
-  <div className="question-container">
-    {isSubmitted ? (
-      <SubmissionList />
-    ) : (
-      <div>
-        {components.length > 0 && (
-          <form onSubmit={handleSubmit}>
-            {questions.map((question) => (
-              <div
-                key={question.name}
-                className={
-                  showDescriptions[question.name]
-                    ? "question-card active"
-                    : "question-card"
-                }
-              >
-                <div
-                  className="question-title"
-                  onClick={() => toggleDescription(question.name)}
-                >
-                  <h2 className="question">{question.name}</h2>
-                  <span className="question-icon">
-                    {showDescriptions[question.name] ? "▲" : "ⓘ"}
-                  </span>
-                </div>
-                {showDescriptions[question.name] && (
-                  <div className="question-description">
-                    <p>{question.description}</p>
-                  </div>
-                )}
-                <div className="question-answer">
-                  <select
-                    className="answer"
-                    name={question.name}
-                    value={answers[question.name]}
-                    onChange={(e) =>
-                      handleAnswerChange(question.name, e.target.value)
-                    }
-                  >
-                    <option value="">Select An Answer</option>
-                    <option value="5">5 - Perfect fit</option>
-                    <option value="4">4 - Good fit</option>
-                    <option value="3">3 - Average fit</option>
-                    <option value="2">2 - Poor fit</option>
-                    <option value="1">1 - No fit</option>
-                  </select>
-                </div>
-              </div>
-            ))}
-            <button type="submit">Submit</button>
-          </form>
-        )}
-      </div>
-    )}
-  </div>
-  <hr />
-</div>
     </div>
   ) : (
     <div className="login-message">
@@ -223,6 +208,7 @@ function App() {
     </div>
   )}
 </div>
+
 
   );
 }
